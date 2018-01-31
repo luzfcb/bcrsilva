143e5226e6334a6ba177634f0bcef61671b26569
Alquimar <alquimars@gmail.com>
2018-01-31 13:39:21 -0300

    Melhorando layouts

--- bcrsilva/produto/forms.py
+++ bcrsilva/produto/forms.py
@@ -4,8 +4,28 @@ from .models import Categoria, Produto
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
-        fields = '__all__'
+        fields = ['nome', 'descricao']
 
+        widgets = {
+            'nome': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Insira o nome'
+                }
+            ),
+            'descricao': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Descreva a categoria',
+                    'style': 'resize: none',
+                    'rows': 5
+                }
+            )
+        }
+
+        # labels = {
+        #     'nome': 'Nome'
+        # }
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
